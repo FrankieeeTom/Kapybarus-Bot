a57859985d6c356b7d1e9ab99ea66ea6becd0ec8
Frankie <106608697+FrankieeeTom@users.noreply.github.com>
2022-11-17 22:56:11 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -32,19 +32,15 @@ async def on_message(message):
             return
         elif user_message == '!blbec <@768852526514569256>':
             await message.channel.send(f'<@768852526514569256> Není blbec {username}! ')
-            return
-            
-            
-    #     if user_message.lower() == '':
-    #        await message.channel.send('{username} ty jsi ale sprostý! <@&956484462613516308> ti ukážou co je to porušení pravidel!')
-    #        return
+            return 
+
+        
+        
         if user_message.lower() == '!rick':
             await message.channel.send('https://cdn.discordapp.com/attachments/970081996527251546/1020634283754397817/rick-astley.gif')
             return
         if user_message.lower() == '<@947562598969245777>':
             await message.channel.send('TO JSEM JÁ!')
             return    
-        if user_message.lower() == 'test':
-            await message.channel.send('/imagine prompt:dog in space')
-            return
+
     client.run(TOKEN)
